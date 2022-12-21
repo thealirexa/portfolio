f0a8e0b6dbdb056ae0706ce80aeece3cbdac6f2a
Alireza Moghaddam <armoghaddam24@gmail.com>
2022-12-22 01:13:18 +0330

    dynamic greetings

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,25 @@
 const typed = document.getElementsByClassName(".typed");
+const greeting = document.getElementById("#greeting");
+
+const greetings = [
+  "Hi 👋",
+  "Hello 👋",
+  "Hey there 👋",
+  "Howdy! 👋",
+  "Just a friendly little hello from me to you 👋",
+  "Hi there! Just felt like sharing a smile with you today 😉",
+  "Hey, you! What's new? 👋",
+  "In the immortal words of Adele, 'Hello…it's me…' 👋",
+  "¡Hola! 👋🤠",
+];
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+var indexOfGreeting = randomNumber(0, greetings.length - 1);
+console.log(indexOfGreeting);
+greeting.innerHTML = greetings[indexOfGreeting];
+
 if (typed) {
   let typed_strings = [
     "Software engineer",
